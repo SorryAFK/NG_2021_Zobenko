d8eb4d1f5cc59dd8a136a21924a5f28747dca889
Pavel Zobenko <79662316+SorryAFK@users.noreply.github.com>
2021-07-09 04:58:59 +0300

    Add files via upload

---
+++ Lesson1Task2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int sum;
+    cout << "Vedite chislo 1:  "<< endl;
+    cin >> num1;
+    cout << "Vedite chislo 2:  " << endl;
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Summa " << sum << endl;
+}
